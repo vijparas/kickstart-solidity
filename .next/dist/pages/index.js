041b4aceb5b5ff32694e7135109f9786856dc1d2
Paras <vijparas@gmail.com>
2018-04-21 21:04:41 +0530

    Created new web3.js file to create new instance of web3 and export it so that instance can be used.

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'D:\\blockchain\\workspace\\kickstart-solidity\\pages\\index.js?entry';
+
+exports.default = function () {
+  return _react2.default.createElement('h1', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, 'This is a campaign home page');
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTyxBQUVQOzs7Ozs7OztrQkFBYyxZQUFJLEFBQ2hCO3lCQUFPLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxHQUFBLEVBQVAsQUFBTyxBQUNSO0FBRkQiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiRDovYmxvY2tjaGFpbi93b3Jrc3BhY2Uva2lja3N0YXJ0LXNvbGlkaXR5In0=
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default()=>{
+  return <h1>This is a campaign home page</h1>;
+}
