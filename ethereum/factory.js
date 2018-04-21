b00562c0157680e97a2e8c92e6cc956796f172a6
Paras <vijparas@gmail.com>
2018-04-21 21:27:08 +0530

    Minor correction in factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,6 +2,6 @@ import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 const campaignFactoryInstance= new web3.eth.Contract(
         JSON.parse(compiledFactory.interface),
-        0x5d506d20B9dE7064a1f445273CA86728B2124B71
+        '0x5d506d20B9dE7064a1f445273CA86728B2124B71'
 );
 export default campaignFactoryInstance;
