430b99261a110a144bef524d3047be938d119d66
Paras <vijparas@gmail.com>
2018-04-24 23:29:12 +0530

    Create Campaign using metamask by setting up submit of form and calling createCampaign method of Campaign Factory

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,24 +1,36 @@
 import React,{Component} from 'react';
 import Layout from '../../components/Layout'
 import {Form,Button,Input} from 'semantic-ui-react'
+import CampaignFactory from '../../ethereum/factory'
+import web3 from '../../ethereum/web3'
 class CampaignNew extends Component{
   state={
     'minimumContribution':''
+  }
+  onSubmit=async (event)=>{
+      console.log("hi");
+      event.preventDefault();
+      const accounts=await web3.eth.getAccounts()
+      await CampaignFactory.methods
+      .createCampaign(this.state.minimumContribution)
+      .send({
+        from:accounts[0]
+      })
   }
   render(){
     return (
       <Layout>
       <h3>Create Campaign</h3>
-      <Form>
-    <Form.Field>
+      <Form onSubmit={this.onSubmit}>
+    <Form.Field >
       <label>Minimum Contribution</label>
       <Input label="wei" labelPosition="right"
        placeholder="Minimum Contribution" value={this.state.minimumContribution} onChange={event=>this.setState({minimumContribution:event.target.value})} />
     </Form.Field>
 
-    <Button type='submit' primary>Submit</Button>
+    <Button type='submit' primary >Create</Button>
   </Form>
-    
+
       </Layout>
     );
   }
