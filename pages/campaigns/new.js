e766f86f1226f9e0e54b72e816c1fd9d6ed745e1
Paras <vijparas@gmail.com>
2018-04-24 23:43:19 +0530

    Error handling in form to display error messages

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,14 +1,15 @@
 import React,{Component} from 'react';
 import Layout from '../../components/Layout'
-import {Form,Button,Input} from 'semantic-ui-react'
+import {Form,Button,Input,Message} from 'semantic-ui-react'
 import CampaignFactory from '../../ethereum/factory'
 import web3 from '../../ethereum/web3'
 class CampaignNew extends Component{
   state={
-    'minimumContribution':''
+    'minimumContribution':'',
+    'error':''
   }
   onSubmit=async (event)=>{
-      console.log("hi");
+    try{
       event.preventDefault();
       const accounts=await web3.eth.getAccounts()
       await CampaignFactory.methods
@@ -16,18 +17,23 @@ class CampaignNew extends Component{
       .send({
         from:accounts[0]
       })
+    }
+    catch(err){
+        this.setState({'err':err.message})
+    }
+
   }
   render(){
     return (
       <Layout>
       <h3>Create Campaign</h3>
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.err}>
     <Form.Field >
       <label>Minimum Contribution</label>
       <Input label="wei" labelPosition="right"
        placeholder="Minimum Contribution" value={this.state.minimumContribution} onChange={event=>this.setState({minimumContribution:event.target.value})} />
     </Form.Field>
-
+    <Message error header="OOPS Something went wrong" content={this.state.err} />
     <Button type='submit' primary >Create</Button>
   </Form>
 
