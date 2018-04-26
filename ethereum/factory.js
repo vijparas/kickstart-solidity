58341dae6fb818aca8dfea65143556b27608d604
Paras <vijparas@gmail.com>
2018-04-26 22:42:55 +0530

    Reployed Campaign and updated address of deployed campaign in out file

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,6 +2,6 @@ import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
 const campaignFactoryInstance= new web3.eth.Contract(
         JSON.parse(CampaignFactory.interface),
-        '0x5039a58461942c2a1C2eA505De88d2809DB119Cd'
+        '0xBd9AE7D3e8772F3E07c95d86f1dB9c898C6Cf7C8'
 );
 export default campaignFactoryInstance;
