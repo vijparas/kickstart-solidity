f827c8f7d085353f9f69ed910009f93b5ea9decc
Paras <vijparas@gmail.com>
2018-04-26 22:33:27 +0530

    Added new function in our Campaign contract to provide summary of campaign.

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -4,7 +4,7 @@ const fs=require('fs-extra');
 
 const buildPath=path.resolve(__dirname,'build');
 // remove entire build directory
-fs.removeSync(buildPath);
+//fs.removeSync(buildPath);
 
 const campaignPath=path.resolve(__dirname,"contracts",'Campaign.sol');
 //rwad contents of the Campaign file
@@ -14,15 +14,23 @@ const source=fs.readFileSync(campaignPath,'utf-8');
 
 const output=solc.compile(source,1).contracts;
 
+console.log(output);
 //  create build directory
 
-fs.ensureDirSync(buildPath);
-//console.log(output);
+//fs.ensureDirSync(buildPath);
+//fs.chmod(buildPath, '777', function(err){
+//     if(err){
+//       console.log("something went wrong");
+//     }
+//     else{
+//       console.log("file permission changes");
+//     }
+// });
 // here we are going to take the compiled output and parse it to save the
 //output into two seperate json files which will contain the code
 // for Campiagn Contract and CampaignFactory contract
  for(let contract in output){
-   
+    console.log("insider for loop");
   fs.outputJsonSync(
   path.resolve(buildPath, contract.replace(':','') + '.json'),
     output[contract]
